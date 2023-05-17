914872982cb5806ad83928d31ad5bfb55b09d647
ANDRES FELIPE MEDINA MUÑOZ <107763296+felipemedina05@users.noreply.github.com>
2023-05-16 22:26:45 -0500

    creando id para colaborador y equipo f eliminar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import {v4 as uuid} from "uuid"
 import './App.css';
 import Header from "./componentes/Header/Header"
 import Formulario from './componentes/Formulario/Formulario';
@@ -9,6 +10,7 @@ import Footer from './componentes/Footer';
 function App() {
   const [mostrarFormulario, actualizarMostrar] = useState(false)
   const [colaboradores, actualizarColaboradores] = useState([{
+    id: uuid(),
     equipo: "Front End",
     foto: "https://github.com/felipemedina05.png",
     nombre: "Andres Medina",
@@ -16,36 +18,43 @@ function App() {
   }])
   const [equipos,actualizarEquipos] =useState( [
     {
+      id: uuid(),
       titulo: "Programación",
       colorPrimario: "#57C278",
       colorSecundario: "#D9F7E9"
     },
     {
+      id: uuid(),
       titulo: "Front End",
       colorPrimario: "#82CFFA",
       colorSecundario: "#E8F8FF"
     },
     {
+      id: uuid(),
       titulo: "Data Science",
       colorPrimario: "#A6D157",
       colorSecundario: "#F0F8E2"
     },
     {
+      id: uuid(),
       titulo: "Devops",
       colorPrimario: "#E06B69",
       colorSecundario: "#FDE7E8"
     },
     {
+      id: uuid(),
       titulo: "UX y Diseño",
       colorPrimario: "#DB6EBF",
       colorSecundario: "#FAE9F5"
     },
     {
+      id: uuid(),
       titulo: "Móvil",
       colorPrimario: "#FFBA05",
       colorSecundario: "#FFF5D9"
     },
     {
+      id: uuid(),
       titulo: "Innovación y Gestión",
       colorPrimario: "#FF8A29",
       colorSecundario: "#FFEEDF"
@@ -67,16 +76,18 @@ function App() {
   }
 
   //Eliminar colaborador
-  const eliminarColaborador = () => {
-    console.log("Eliminar colaborador")
+  const eliminarColaborador = (id) => {
+    console.log("Eliminar colaborador", id)
+  const nuevoColaborador = colaboradores.filter ((colaborador) => colaborador.id !== id)
+  actualizarColaboradores(nuevoColaborador)
   }
 
   //actualizar color de equipos
 
-  const actualizarColor = (color,titulo) => {
-    console.log ("nuevo color ", color , titulo)
+  const actualizarColor = (color,id) => {
+    console.log ("nuevo color ", color , id)
     const equiposActualizados = equipos.map((equipo) => {
-      if(equipo.titulo === titulo) {equipo.colorPrimario = color}
+      if(equipo.id === id) {equipo.colorPrimario = color}
       return equipo
     })
     actualizarEquipos(equiposActualizados)
